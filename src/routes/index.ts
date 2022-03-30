6151542b52ac2531594cc1b8d974c5a71f14a372
Jose A <jos.alv.itm@hotmail.com>
2022-03-30 16:29:41 -0600

    remove extra calls

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,5 @@
 import { Router } from "express";
 import HomeRouter from "./HomeRouter";
-import PhotoRouter from "./PhotoRouter";
-import UserRouter from "./UserRouter";
-import AuthRouter from "./AuthRouter";
 
 /**
  * Main router for our express application
