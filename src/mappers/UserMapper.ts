19e0d31236564dccafd19a3fc22b272aa769e2cb
Jose A <jos.alv.itm@hotmail.com>
2022-03-31 14:14:50 -0600

    UserMapper added

--- src/mappers/UserMapper.ts
+++ src/mappers/UserMapper.ts
@@ -1,2 +1,19 @@
+import User from "../entities/User";
+import { IMapper } from "./IMapper";
 
+export interface UserDto {
+  id: number;
+  email: string;
+}
 
+/**
+ * User Mapper for serializing.
+ */
+export class UserMapper implements IMapper<User, UserDto> {
+  toDto(u: User): UserDto {
+    return {
+      id: u.id,
+      email: u.email,
+    };
+  }
+}
