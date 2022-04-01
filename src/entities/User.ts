cf424857ce298f1dbbbada0b373233b67399519c
juandavid <juandavidalvarez52@gmail.com>
2022-04-01 11:43:09 -0500

    fix merge conlfict in user entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,6 +3,15 @@ import {
   Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, 
 } from "typeorm";
 
+export enum UserRole {
+  ADMIN = "ADMIN",
+  DEVELOPER = "DEVELOPER",
+  DESIGNER = "DESIGNER",
+  PM = "PROJECT MANAGER",
+  SM = "SCRUM MASTER",
+  HR = "HR",
+  CONTRACTOR = "CONTRACTOR",
+}
 
 @Entity()
 export default class User {
