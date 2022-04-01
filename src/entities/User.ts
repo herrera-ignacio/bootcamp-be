2dd090d43514a6ff1d41630392b356bf98696577
Jose A <jos.alv.itm@hotmail.com>
2022-04-01 02:04:08 -0600

    user entity and validators update

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -3,6 +3,15 @@ import {
   Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn, 
 } from "typeorm";
 
+export enum UserRole {
+  ADMIN = "ADMIN",
+  DEVELOPER = "DEVELOPER",
+  DESIGNER = "DESIGNER",
+  PM = "PROJECT MANAGER",
+  SM = "SCRUM MASTER",
+  HR = "HR",
+  CONTRACTOR = "CONTRACTOR",
+}
 
 @Entity()
 export default class User {
@@ -18,4 +27,15 @@ export default class User {
   @Column({ type: "varchar", unique: true })
   @IsEmail()
     email: string;
+  
+  @Column({ type: "varchar", nullable: true })
+    firstName: string;
+
+  @Column({ type: "varchar", nullable: true })
+    lastName: string;
+
+  @Column({
+    default: UserRole.CONTRACTOR,
+  })
+    role: UserRole;
 }
