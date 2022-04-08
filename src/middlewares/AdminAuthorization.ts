99177d787e7450abdc53afcbfa89cc0193208c47
juandavid <juandavidalvarez52@gmail.com>
2022-04-08 09:30:13 -0500

    Remove console logs in AdminAuthorization Middleware

--- src/middlewares/AdminAuthorization.ts
+++ src/middlewares/AdminAuthorization.ts
@@ -1,6 +1,5 @@
 import UserService from "../services/UserService";
 import { UserRole } from "../entities/User";
-import Log from "../utils/Log";
 import HttpException from "../exceptions/HttpException";
 import { IRequestHandler } from "../types/IRequestHandler";
 
@@ -13,15 +12,10 @@ const AdminAuthorization = ( // eslint-disable-line @typescript-eslint/no-explic
     const userService = new UserService(); 
 
     if (req) {
-      Log.info(req.user);
-      console.log(req.user);
-      console.log(req.auth);
       const user = await userService.getByEmail(req.user.email);
       if (user.role === UserRole.ADMIN) {
-        Log.info(JSON.stringify(user));
         next();
       } else {
-        Log.info(user.role);
         next(new HttpException(403, "Forbidden"));
       }
       
