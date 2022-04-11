79d538e9fd7f7577ed71e4223417a380f78f41ac
juandavid <juandavidalvarez52@gmail.com>
2022-04-11 12:37:24 -0500

    fix config file

--- src/providers/Config.ts
+++ src/providers/Config.ts
@@ -16,8 +16,8 @@ type AppConfig = {
   databaseHost: string;
   databasePort: number;
   databaseName: string;
-  databaseSynchronize: boolean;
-  databaseLogging: boolean;
+  isDatabaseSynchronize: boolean;
+  isDatabaseLogging: boolean;
   auth0ServerAudience: string;
   auth0ClientAudience: string;
   auth0Issuer: string;
@@ -35,25 +35,34 @@ class Config implements IMiddleware {
    */
   public static config(): AppConfig {
     if (!Config.appConfig) {
-      dotenv.config({ path: path.join(__dirname, "../../.env") });
+      dotenv.config({
+        path: path.join(
+          __dirname, "../../.env",
+        ),
+      });
       Config.appConfig = {
-        appName: process.env.APP_NAME || "Uncharted-scape-pods",
-        appDescription: process.env.APP_DESCRIPTION || "API for Agile Engine employees to make reservations",
-        port: !Number.isNaN(Number(process.env.PORT)) ? Number(process.env.PORT) : 4000,
-        isCORSEnabled: Boolean(process.env.CORS_ENABLED),
-        appURL: process.env.APP_URL || "localhost",
-        apiPrefix: process.env.API_PREFIX || "",
-        databaseUser: process.env.DATABASE_USERNAME || "postgres",
-        databasePassword: process.env.DATABASE_PASSWORD || "postgres",
-        databaseHost: process.env.DATABASE_HOST || "localhost",
-        databasePort: !Number.isNaN(Number(process.env.DATABASE_PORT)) ? Number(process.env.DATABASE_PORT) : 5432,
-        databaseName: process.env.DATABASE_NAME || "mydb",
-        databaseLogging: process.env.DATABASE_LOGGING?.toLowerCase() === "true",
-        databaseSynchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
-        auth0ServerAudience: process.env.AUTH0_SERVER_AUDIENCE,
+        apiPrefix     : process.env.API_PREFIX || "",
+        appDescription: process.env.APP_DESCRIPTION
+        || "API for Agile Engine employees to make reservations",
+        appName            : process.env.APP_NAME || "Uncharted-scape-pods",
+        appURL             : process.env.APP_URL || "localhost",
         auth0ClientAudience: process.env.AUTH0_CLIENT_AUDIENCE,
-        auth0Issuer: process.env.AUTH0_ISSUER,
-        auth0jwksUri: process.env.AUTH0_JWKS_URI,
+        auth0Issuer        : process.env.AUTH0_ISSUER,
+        auth0jwksUri       : process.env.AUTH0_JWKS_URI,
+        auth0ServerAudience: process.env.AUTH0_SERVER_AUDIENCE,
+        databaseHost       : process.env.DATABASE_HOST || "localhost",
+        databaseName       : process.env.DATABASE_NAME || "mydb",
+        databasePassword   : process.env.DATABASE_PASSWORD || "postgres",
+        databasePort       : !Number.isNaN(Number(process.env.DATABASE_PORT))
+          ? Number(process.env.DATABASE_PORT)
+          : 5432,
+        databaseUser         : process.env.DATABASE_USERNAME || "postgres",
+        isCORSEnabled        : Boolean(process.env.CORS_ENABLED),
+        isDatabaseLogging    : process.env.DATABASE_LOGGING?.toLowerCase() === "true",
+        isDatabaseSynchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+        port                 : !Number.isNaN(Number(process.env.PORT))
+          ? Number(process.env.PORT)
+          : 4000,
       };
     }
 
