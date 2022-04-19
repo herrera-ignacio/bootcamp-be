bb3865a5d672f3ea059743701b4b9d3bad7861a2
Gabriel V. Santana <gabrielsantana.it@gmail.com>
2022-04-19 09:29:54 -0300

    Added tests for getAll EP rooms

--- src/controllers/RoomController.test.ts
+++ src/controllers/RoomController.test.ts
@@ -18,6 +18,49 @@ describe(
 
     afterEach(() => sandbox.restore());
 
+    it(
+      "getAll should return 200 and get a list of all rooms", async () => {
+        // Given
+        const roomsMock = [ getRoomMock(), getRoomMock(), getRoomMock() ];
+        const fakeService = sinon.createStubInstance(RoomService);
+        const fakeReq = getRequestMock();
+        const fakeRes = getResponseMock();
+        const controller = new RoomController(fakeService);
+
+        // When
+        fakeService.getAll.resolves(roomsMock);
+
+        await controller.getAll(
+          fakeReq, fakeRes as any, null,
+        );
+
+        // Then
+        expect(fakeService.getAll.called).toBeTruthy();
+        expect(fakeRes.json.calledWith(sinon.match({
+          data: roomsMock.map((room) => new RoomMapper().toDto(room)),
+        }))).toBeTruthy();
+        expect(fakeRes.status.calledOnceWithExactly(200)).toBeTruthy();
+      },
+    );
+
+    it(
+      "getAll should bubble up exception", async () => {
+        // Given
+        const fakeService = sinon.createStubInstance(RoomService);
+        const fakeReq = getRequestMock();
+        const fakeRes = getResponseMock();
+        const controller = new RoomController(fakeService);
+
+        // When
+        fakeService.getAll.throws();
+
+        // Then
+        await expect(controller.getAll(
+          fakeReq, fakeRes as any, null,
+        )).rejects.toThrow();
+        expect(fakeService.getAll.calledOnce).toBeTruthy();
+      },
+    );
 
     it(
       "create should return 201 and room on success", async () => {
@@ -44,7 +87,6 @@ describe(
         );
 
         // Then
-
         expect(fakeService.create.calledOnceWithExactly(roomCreateBody)).toBeTruthy();
         expect(fakeRes.json.calledOnceWithExactly({
           data: new RoomMapper().toDto(roomMock),
