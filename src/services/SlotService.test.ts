f8b44d022876c1b80248ded046d69196e66c82f0
Gabriel V. Santana <gabrielsantana.it@gmail.com>
2022-04-22 16:26:40 -0300

    Included tests for create slots functionality

---
+++ src/services/SlotService.test.ts
@@ -0,0 +1,58 @@
+import sinon from "sinon";
+import { stubInterface } from "ts-sinon";
+import Slot from "../entities/Slot";
+import IRepository from "../types/IRepository";
+import getSlotMock from "../mocks/SlotMock";
+import SlotService from "./SlotService";
+
+describe(
+  "SlotService", () => {
+    const sandbox = sinon.createSandbox();
+
+    afterEach(() => sandbox.restore());
+
+    it(
+      "create should generate a new slot when called and return its data", async () => {
+        // Given
+        const slotMock = getSlotMock();
+        const fakeRepo = stubInterface<IRepository<Slot>>();
+        const userInput = {
+          isDisabled: true,
+        };
+
+        // When
+        fakeRepo.save.resolves(slotMock);
+        sandbox.replace(
+          SlotService.prototype, "getRepository", () => fakeRepo,
+        );
+
+        // Then
+        const res = await new SlotService().create(userInput);
+
+        expect(res).toEqual(slotMock);
+        expect(fakeRepo.save.calledWithExactly(userInput)).toBeTruthy();
+      },
+    );
+
+    it(
+      "create fails due to missing parameters", async () => {
+        // Given
+        const fakeRepo = stubInterface<IRepository<Slot>>();
+
+        // When
+        fakeRepo.save.throws();
+        sandbox.replace(
+          SlotService.prototype, "getRepository", () => fakeRepo,
+        );
+
+        // Then
+        await expect(new SlotService().create({
+          isDisabled: undefined,
+        }))
+          .rejects.toThrow();
+
+        expect(fakeRepo.save.calledOnce).toBeTruthy();
+      },
+    );
+  },
+);
